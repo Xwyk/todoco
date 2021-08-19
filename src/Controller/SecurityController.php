0d0ce9f79968a675e7426f2391c828ec545c9808
florianleboul@gmail.com <florianleboul@gmail.com>
2021-08-19 11:27:49 +0200

    Authenticationutils

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -6,16 +6,18 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
     /**
      * @Route("/login", name="login")
      */
-    public function loginAction(Request $request)
+    public function loginAction(Request $request, AuthenticationUtils $authenticationUtils)
     {
-        $authenticationUtils = $this->get('security.authentication_utils');
-
+        if ($this->getUser()) {
+            return $this->redirectToRoute('home');
+        }
         $error = $authenticationUtils->getLastAuthenticationError();
         $lastUsername = $authenticationUtils->getLastUsername();
 

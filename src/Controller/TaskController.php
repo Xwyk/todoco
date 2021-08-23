59fff1af0721d4eb52b35012d9650cf034518711
florianleboul@gmail.com <florianleboul@gmail.com>
2021-08-23 13:18:14 +0200

    Controller correct & update & use dependacie injection

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -2,6 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Task;
+use App\Form\TaskType;
+use App\Repository\TaskRepository;
+use Doctrine\ORM\EntityManager;
+use phpDocumentor\Reflection\Types\Iterable_;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -12,66 +18,68 @@ class TaskController extends AbstractController
     // @TODO add view task route
     /**
      * @Route("/tasks", name="task_list")
+     * @isGranted("TASKS_VIEW")
+     * @param TaskRepository $repository
+     * @return Response
      */
-    public function listAction()
+    public function list(TaskRepository $repository): Response
     {
-        return $this->render('task/list.html.twig', ['tasks' => $this->getDoctrine()->getRepository('App:Task')->findAll()]);
+        return $this->render('task/list.html.twig', ['tasks' => $repository->findAll()]);
     }
 
     /**
      * @Route("/tasks/create", name="task_create")
+     * @isGranted("ROLE_USER")
+     * @param Request $request
+     * @param EntityManager $manager
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|Response
+     * @throws \Doctrine\ORM\ORMException
+     * @throws \Doctrine\ORM\OptimisticLockException
      */
-    public function createAction(Request $request)
+    public function create(Request $request, EntityManager $manager)
     {
         $task = new Task();
         $form = $this->createForm(TaskType::class, $task);
-
         $form->handleRequest($request);
-
-        if ($form->isValid()) {
-            $em = $this->getDoctrine()->getManager();
-
-            $em->persist($task);
-            $em->flush();
-
+        if ($form->isSubmitted() && $form->isValid()) {
+            $manager->persist($task);
+            $manager->flush();
             $this->addFlash('success', 'La tâche a été bien été ajoutée.');
-
             return $this->redirectToRoute('task_list');
         }
-
         return $this->render('task/create.html.twig', ['form' => $form->createView()]);
     }
 
     /**
      * @Route("/tasks/{id}/edit", name="task_edit")
+     * @isGranted("TASK_EDIT", subject="task")
+     * @param Task $task
+     * @param Request $request
+     * @param EntityManager $manager
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|Response
+     * @throws \Doctrine\ORM\ORMException
+     * @throws \Doctrine\ORM\OptimisticLockException
      */
-    public function editAction(Task $task, Request $request)
+    public function edit(Task $task, Request $request, EntityManager $manager)
     {
         $form = $this->createForm(TaskType::class, $task);
-
         $form->handleRequest($request);
-
-        if ($form->isValid()) {
-            $this->getDoctrine()->getManager()->flush();
-
+        if ($form->isSubmitted() && $form->isValid()) {
+            $manager->flush();
             $this->addFlash('success', 'La tâche a bien été modifiée.');
-
             return $this->redirectToRoute('task_list');
         }
-
-        return $this->render('task/edit.html.twig', [
-            'form' => $form->createView(),
-            'task' => $task,
-        ]);
+        return $this->render('task/edit.html.twig', ['form' => $form->createView(), 'task' => $task,]);
     }
 
     /**
      * @Route("/tasks/{id}/toggle", name="task_toggle")
+     * @isGranted("TASK_TOGGLE", subject="task")
      */
-    public function toggleTaskAction(Task $task)
+    public function toggle(Task $task, EntityManager $manager)
     {
-        $task->toggle(!$task->isDone());
-        $this->getDoctrine()->getManager()->flush();
+        $task->setIsDone(!$task->getIsDone());
+        $manager->flush();
 
         $this->addFlash('success', sprintf('La tâche %s a bien été marquée comme faite.', $task->getTitle()));
 
@@ -80,12 +88,17 @@ class TaskController extends AbstractController
 
     /**
      * @Route("/tasks/{id}/delete", name="task_delete")
+     * @isGranted("TASK_DELETE", subject="task")
+     * @param Task $task
+     * @param EntityManager $manager
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     * @throws \Doctrine\ORM\ORMException
+     * @throws \Doctrine\ORM\OptimisticLockException
      */
-    public function deleteTaskAction(Task $task)
+    public function delete(Task $task, EntityManager $manager)
     {
-        $em = $this->getDoctrine()->getManager();
-        $em->remove($task);
-        $em->flush();
+        $manager->remove($task);
+        $manager->flush();
 
         $this->addFlash('success', 'La tâche a bien été supprimée.');
 

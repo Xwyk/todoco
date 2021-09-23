ca9ed013558bd085a697b824ad32fbb1496c72bc
florianleboul@gmail.com <florianleboul@gmail.com>
2021-09-24 01:56:55 +0200

    Add task anonymous

--- src/DataFixtures/TaskFixtures.php
+++ src/DataFixtures/TaskFixtures.php
@@ -16,14 +16,16 @@ class TaskFixtures extends Fixture implements OrderedFixtureInterface
         $task2 = $this->createTask('Task 2', 'Description 2', $this->getReference('user2'));
         $task3 = $this->createTask('Task 3', 'Description 3', $this->getReference('admin1'));
         $task4 = $this->createTask('Task 4', 'Description 4', $this->getReference('admin2'));
+        $task5 = $this->createTask('Task 4', 'Description 4');
         $manager->persist($task1);
         $manager->persist($task2);
         $manager->persist($task3);
         $manager->persist($task4);
+        $manager->persist($task5);
         $manager->flush();
     }
 
-    protected function createTask(string $title, string $content, User $author): Task{
+    protected function createTask(string $title, string $content, User $author = null): Task{
         $task = new Task();
         $task->setTitle($title)
             ->setContent($content)

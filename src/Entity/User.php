14078e178918bfe6299a19f5ac9dc1790bb1b3d3
Florian LEBOUL <florianleboul@gmail.com>
2021-10-21 13:10:34 +0200

    unicity on username added

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -6,12 +6,15 @@ use App\Repository\UserRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=UserRepository::class)
  * @ORM\Table(name="`user`")
+ * @UniqueEntity("username")
  *
  * @method string getUserIdentifier()
  */
@@ -26,6 +29,7 @@ class User implements PasswordAuthenticatedUserInterface, UserInterface
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\Unique()
      */
     private $username;
 
